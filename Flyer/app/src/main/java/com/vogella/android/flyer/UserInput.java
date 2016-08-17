777f6d42cf3fe0d3e82e44c9bc3a3857723d2ba1
AmritaC <achowdhury@storytree.me>
2016-08-17 14:15:55 -0700

    allow country code as valid phone number

--- Flyer/app/src/main/java/com/vogella/android/flyer/UserInput.java
+++ Flyer/app/src/main/java/com/vogella/android/flyer/UserInput.java
@@ -16,6 +16,8 @@ public class UserInput {
     public final static String URL_INPUT = "businessId";
     public final static String BAD_INPUT = "badInput";
     private final static String TAG = "USER_INPUT";
+    private final static char US_COUNTRY_CODE = '1';
+    private final static String US_COUNTRY_CODE_FORMATTED = "+1";
 
     public String getInput() {
         return input;
@@ -28,46 +30,25 @@ public class UserInput {
     private String inputType;
 
     public UserInput(String inputString) {
-        if (isValidPhone(inputString)) {
-            this.input = formatPhoneNumber(inputString);
-            this.inputType = PHONE_INPUT;
-        } else if (isValidUrl(inputString)) {
+        if (isValidUrl(inputString)) {
             this.input = getBusinessIdFromUrl(inputString);
             this.inputType = URL_INPUT;
         } else {
-            this.input = null;
-            this.inputType = BAD_INPUT;
+            inputString = removeCountryCode(inputString);
+            if (isValidPhone(inputString)) {
+                this.input = formatPhoneNumber(inputString);
+                this.inputType = PHONE_INPUT;
+            } else {
+                this.input = null;
+                this.inputType = BAD_INPUT;
+            }
         }
-    }
-
-    private boolean isValidPhone(String input) {
-        // allowing +countrycode in phone as valid and regex doesn't include that.
-        if (input.length() < 10) {
-            return false;
-        }
-        input = input.charAt(0) == '+'
-                ? input.substring(2, input.length())
-                : input;
-        Pattern pattern = Pattern.compile(PHONE_REGEX);
-        Matcher matcher = pattern.matcher(input.replace(" ",""));
-        return matcher.matches();
     }
 
     private boolean isValidUrl(String input) {
         return (input.startsWith("https://www.yelp.com/biz/") ||
                 input.startsWith("http://www.yelp.com/biz/") ||
-        input.startsWith("www.yelp.com/biz/"));
-    }
-
-    private String formatPhoneNumber(String phone) {
-        phone = phone.charAt(0) == '+' ? phone.substring(2, phone.length()) : phone;
-        String formattedPhone = "+1";
-        for (char s : phone.toCharArray()) {
-            if (Character.isDigit(s)) {
-                formattedPhone += s;
-            }
-        }
-        return formattedPhone;
+                input.startsWith("www.yelp.com/biz/"));
     }
 
     private String getBusinessIdFromUrl(String yelpUrl) {
@@ -81,5 +62,38 @@ public class UserInput {
         }
         return "";
     }
+
+    private String removeCountryCode(String phone) {
+        phone = phone.replace(" ","");
+        if (phone.length() <= 10) {
+            return phone;
+        }
+        else if (phone.charAt(0) == '+') { //+17652772890
+            return phone.substring(2, phone.length());
+        }
+        else if (phone.charAt(0) == US_COUNTRY_CODE && phone.charAt(3) == '.') {
+            return phone; //176.566.2567 - here 1 is not country code
+        }
+        else {
+            return phone.substring(1, phone.length());
+        }
+    }
+
+    private boolean isValidPhone(String input) {
+        Pattern pattern = Pattern.compile(PHONE_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    private String formatPhoneNumber(String phone) {
+        String formattedPhone = US_COUNTRY_CODE_FORMATTED;
+        for (char s : phone.toCharArray()) {
+            if (Character.isDigit(s)) {
+                formattedPhone += s;
+            }
+        }
+        return formattedPhone;
+    }
+    
 }
 
